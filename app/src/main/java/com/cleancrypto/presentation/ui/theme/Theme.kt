6ce7024f45fe8cfd40c6552a5d5cd6a15e7efb52
Prasanna <github@prasansrini.com>
2024-05-03 17:46:28 +0200

    Removed unused themes.

--- app/src/main/java/com/cleancrypto/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/cleancrypto/presentation/ui/theme/Theme.kt
@@ -1,17 +1,9 @@
 package com.cleancrypto.presentation.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-
-private val DarkColorScheme = darkColorScheme(
-	primary = ColorPrimary,
-	background = Color.White,
-	onBackground = MediumGray,
-	onPrimary = DarkGray
-)
 
 private val LightColorScheme = lightColorScheme(
 	primary = ColorPrimary,
